733b6d2b4e2b0a555ca56c3dc8075b9f579905fc
Maksym <maksym.045z@gmail.com>
2025-10-26 17:33:50 +0100

    removed domain and samesite to lax, another approach for origins

--- src/main/java/org/easeport/itsupportsystem/config/WebSecurityConfig.java
+++ src/main/java/org/easeport/itsupportsystem/config/WebSecurityConfig.java
@@ -65,8 +65,8 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        List<String> originList = Arrays.asList(allowedOrigin.split(","));
-        config.setAllowedOrigins(List.of(originList.getFirst(), originList.getLast())); // frontend origin
+
+        config.setAllowedOrigins(List.of(allowedOrigin)); // frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true); // important for cookies
         config.setAllowedHeaders(List.of("*")); // allow all headers
--- src/main/java/org/easeport/itsupportsystem/controller/AuthController.java
+++ src/main/java/org/easeport/itsupportsystem/controller/AuthController.java
@@ -62,12 +62,11 @@ public class AuthController {
         UserPrincipal userDetails = (UserPrincipal) authentication.getPrincipal();
 
         ResponseCookie cookie = ResponseCookie.from("token", jwt)
-                .domain("easeport.org")
                 .httpOnly(true)
                 .secure(true) // HTTPS
                 .path("/")
                 .maxAge(7 * 24 * 60 * 60) // 7 days
-                .sameSite("None")
+                .sameSite("Lax")
                 .build();
         response.addHeader("Set-Cookie", cookie.toString());
 
