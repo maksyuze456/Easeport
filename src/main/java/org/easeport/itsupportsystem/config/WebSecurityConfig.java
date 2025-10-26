e5381269e975cda9943ed1df5ba12133ab7a81ed
Maksym <maksym.045z@gmail.com>
2025-10-26 13:54:39 +0100

    origins split by comma

--- src/main/java/org/easeport/itsupportsystem/config/WebSecurityConfig.java
+++ src/main/java/org/easeport/itsupportsystem/config/WebSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -64,7 +65,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigin)); // frontend origin
+        config.setAllowedOrigins(Arrays.asList(allowedOrigin.split(","))); // frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true); // important for cookies
         config.setAllowedHeaders(List.of("*")); // allow all headers
